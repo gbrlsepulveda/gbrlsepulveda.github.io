85a9e7af9c970c37d44a317b042b6d3dd65889e7
Gabriel Sepulveda <gabrielsepulveda07@gmail.com>
2015-09-01 21:18:02 -0300

    folder structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,192 +2,103 @@
 
 module.exports = function(grunt) {
 
-    // Load all tasks
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-    require('time-grunt')(grunt);
+  // Load all tasks
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  require('time-grunt')(grunt);
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        // Concat and minify javascripts
-        uglify: {
-            options: {
-                compress: {
-                    drop_console: true
-                }
-            },
-            dist: {
-                files: {
-                    '_site/assets/scripts/app.min.js': [
-                        'src/assets/scripts/plugins.js',
-                        'src/assets/scripts/main.js'
-                    ]
-                }
-            }
-        },
-
-        clean: {
-            dist: {
-                src: ["_site/"]
-            }
-        },
-
-        concat: {
-            dist: {
-                files: {
-                    '_site/assets/scripts/app.min.js': [
-                        'src/assets/scripts/plugins.js',
-                        'src/assets/scripts/main.js'
-                    ]
-                }
-            },
-        },
-
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/assets/',
-                    src: [
-                        'fonts/**/*',
-                        'images/**/*',
-                    ],
-                    dest: '_site/assets/',
-                    dot: true
-                }]
-            }
-        },
-
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/',
-                    src: '*.html',
-                    dest: '_site',
-                }],
-            }
-        },
-
-        imagemin: {
-            dist: {
-                options: {
-                    optimizationLevel: 3
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/images',
-                    src: ['*.{png,jpg,gif}'],
-                    dest: '_site'
-                }],
-            }
-        },
-
-        // concurrent: {
-        //     dev: ['watch', 'connect']
-        // },
-
-        csslint: {
-            options: {
-                csslintrc: '.csslintrc'
-            },
-            src: ['_siteassets/styles/*.css']
-        },
-
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            all: ['src/assets/scripts/*.js']
-        },
-
-        jekyll: {
-            options: {
-                config: 'src/jekyll/_config.yml',
-                layouts: 'src/jekyll/_layouts/'
-            },
-            dist: {
-                options: {
-                    src: 'src/jekyll/',
-                    dest: '_site/'
-                }
-            }
-        },
-
-        sass: {
-            options: {
-                sourceMap: true,
-                outputStyle: 'compressed'
-            },
-            dist: {
-                files: {
-                    '_site/assets/styles/screen.css': 'src/assets/styles/screen.scss'
-                }
-            }
-        },
-
-        connect: {
-            server: {
-                options: {
-                    open: true,
-                    port: 8080,
-                    hostname: '0.0.0.0',
-                    base: '_site',
-                    livereload: true
-                }
-            }
-        },
-
-        // Watch
-        watch: {
-            options: {
-                livereload: true
-            },
-            copy: {
-                files: [
-                    'src/assets/fonts/**/*',
-                    'src/assets/images/**/*'
-                ],
-                tasks: ['newer:copy']
-            },
-            js: {
-                files: 'src/assets/scripts/**/*',
-                tasks: ['concat:dist']
-            },
-            jekyll: {
-                files: [
-                    'src/jekyll/**/*'
-                ],
-                tasks: ['build']
-            },
-            sass: {
-                files: ['src/assets/styles/**/*'],
-                tasks: ['sass']
-            },
-            grunt: {
-                files: ['Gruntfile.js']
-            }
+    // Concat and minify javascripts
+    uglify: {
+      options: {
+        compress: {
+          drop_console: true
         }
-    });
+      },
+      dist: {
+        files: {
+          'assets/app.min.js': [
+            'src/assets/scripts/plugins.js',
+            'src/assets/scripts/main.js'
+          ]
+        }
+      }
+    },
 
-    /* Tasks */
+    concat: {
+      dist: {
+        files: {
+          'assets/app.min.js': [
+            'src/assets/scripts/plugins.js',
+            'src/assets/scripts/main.js'
+          ]
+        }
+      },
+    },
 
-    grunt.registerTask('dev', ['build', 'connect:server', 'watch']);
+    imagemin: {
+      dist: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/assets/images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'assets/'
+        }],
+      }
+    },
 
-    grunt.registerTask('lint', ['csslint', 'jshint:all']);
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['src/assets/scripts/*.js']
+    },
 
-    // grunt.registerTask('deploy', ['']);
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'assets/screen.css': 'src/assets/styles/screen.scss'
+        }
+      }
+    },
 
-    grunt.registerTask('build', [
-        'clean',
-        'jekyll',
-        'sass',
-        'copy:dist',
-        'uglify:dist',
-        'htmlmin:dist',
-        'imagemin:dist'
-    ]);
+    watch: {
+      imagemin: {
+        files: [
+          'src/assets/images/**/*'
+        ],
+        tasks: ['imagemin']
+      },
+      js: {
+        files: 'src/assets/scripts/**/*',
+        tasks: ['concat:dist']
+      },
+      sass: {
+        files: ['src/assets/styles/**/*'],
+        tasks: ['sass']
+      },
+      grunt: {
+        files: ['Gruntfile.js']
+      }
+    }
+  });
+
+  /* Tasks */
+
+  grunt.registerTask('default', ['build', 'watch']);
+  grunt.registerTask('dev', ['build', 'watch']);
+
+  grunt.registerTask('lint', ['jshint:all']);
+
+  grunt.registerTask('build', [
+    'sass',
+    'uglify:dist',
+    'imagemin:dist'
+  ]);
 };
---
+++ _posts/2015-06-02-welcome-to-my-blog.md
@@ -0,0 +1,15 @@
+---
+layout: post
+title:  "Foo bar"
+date:   2015-04-04 22:23:59
+categories: general
+---
+
+Olá,
+há muito tempo venho planejando montar este blog, foi muito tempo procastinando e enfim este blog está em produção. Ainda não estou satisfeito, provelmente nunca estarei, mas decidi colocar em produção e aos poucos melhorar certos pontos.
+
+Isso tudo é muito novo pra mim, sempre tive interesse de dar um pontapé inicial na vida de blogueiro, demorou mais do que o esperado, mas enfim chegou a hora. Pretendo escrever sobre coisas que estou aprendendo, tecnologias do meu cotidiano e pretensões de fututo. Não sou um especialista em nenhuma tecnologia, mas sempre tive a vontade de dividir meu conhecimento e ajudar pessoas que estão trilhando o mesmo caminho.
+
+Meu nome é Gabriel Sepulveda, trabalho com web desde 2010, mas em 2006 escrevi minhas primeiras linhas de HTML. Hoje trabalho como Front-end Engineer na B2W Digital, cuidando dos maiores e-commerce's (Americanas.com, Submarino, Shoptime e Sou Barato) da America Latina.
+
+Dentro desde blog meu foco vai ser web.
