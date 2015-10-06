3c92c8a6846612889ce1cc5a11b69c6bdcf3d51c
Gabriel Sepulveda <gabrielsepulveda07@gmail.com>
2015-10-05 22:09:37 -0300

    improved navigation

--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -1,45 +1,41 @@
-;(function(window, document) {
+(function(window, document) {
 
-    'use strict';
+  'use strict';
 
-    var Portifa = (function() {
+  var Portifa = (function() {
 
-        var app = {
-            html: document.body.parentNode,
-            logo: document.querySelector('h1.title a'),
-            nav: document.querySelector('#nav'),
-            navButton: document.querySelector('.header .nav-button')
-        };
+    var app = {
+      html: document.body.parentNode,
+      logo: document.querySelector('h1.title a'),
+      nav: document.querySelector('#nav'),
+      navButton: document.querySelector('.header .nav-button')
+    };
 
-        app.bindEvents = function() {
-            app.navButton.addEventListener('click', function(event) {
-                event.preventDefault();
-                this.classList.toggle('is-active');
+    app.bindEvents = function() {
+      app.navButton.addEventListener('click', function(event) {
+        event.preventDefault();
 
-                if (window.pageYOffset > 150) {
-                    app.nav.classList.add('is-active');
-                } else {
-                    app.nav.classList.toggle('is-active');
-                }
-                window.scroll(0,0);
-            });
+        app.html.classList.toggle('navbar-opened');
 
-            window.addEventListener('scroll', function() {
-                if (window.pageYOffset > 150) {
-                    app.html.classList.add('is-menufloated');
-                } else {
-                    app.html.classList.remove('is-menufloated');
-                }
-            });
-        };
+        window.scroll(0, 0);
+      });
 
-        app.init = function() {
-            this.bindEvents();
-        };
+      // window.addEventListener('scroll', function() {
+      //   if (window.pageYOffset > 150) {
+      //     app.html.classList.add('is-menufloated');
+      //   } else {
+      //     app.html.classList.remove('is-menufloated');
+      //   }
+      // });
+    };
 
-        return app;
-    })();
+    app.init = function() {
+      this.bindEvents();
+    };
 
-    Portifa.init();
+    return app;
+  })();
+
+  Portifa.init();
 
 })(window, document);
